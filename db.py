9dad350eadf459f986d2f2751e75ac4c86396beb
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-24 16:13:39 -0300

    adding deployment yml file

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import os
 import pymysql
 from flask import jsonify
 
+
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
 db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
